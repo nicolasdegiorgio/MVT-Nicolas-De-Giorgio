b1abf1e184f289fa2acb43625a803c537abfe3d1
DeGiorgio Nicolas <nicolasdegiorgio@gmail.com>
2022-07-30 17:02:48 -0300

    Version funcional con Template: 0.2

--- This_fam/Familiares/views.py
+++ This_fam/Familiares/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from Familiares.models import listado_familiares
+from django.template import Template, Context, loader
+
+
 # Create your views here.
 
 
@@ -18,6 +21,32 @@ def lista_familiares(request):
 
 
 
+# def string_familia(request):
+    
+#     familia = listado_familiares.objects.all()
+    
+#     listado= []
+#     for count, items in enumerate(familia):
+        
+#         listado.append(f'El familiar n° {count+1} se llama {items.nombre} {items.apellido}, y su contacto es {items.email}')
+#         return listado
+#     #Hacemos un render para tomar la plantilla
+    
+#     datos = {'Familiares': listado}
+    
+#     #Creamos la plantilla
+    
+#     plantilla=loader.get_template('Familia/index.html')
+    
+    
+#     documento=plantilla.render(datos)
+    
+#     return HttpResponse(documento)
+        
+        
+#     #return HttpResponse (listado)
+   
+   
 def string_familia(request):
     
     familia = listado_familiares.objects.all()
@@ -26,7 +55,22 @@ def string_familia(request):
     for count, items in enumerate(familia):
         
         listado.append(f'El familiar n° {count+1} se llama {items.nombre} {items.apellido}, y su contacto es {items.email}')
-        a = print(listado[items])
-        
-    return HttpResponse (a)
-   
+        #return listado  
+    
+    datos_contexto = {'Familiares': listado}
+    
+    archivo = open(r'G:\Mi unidad\Curso Python\Clase 18 - Django II\Desafio entregable\This_fam\Familiares\templates\index.html', 'r')
+    contenidohtml=archivo.read()
+    archivo.close()
+    
+    #crear la plantilla
+    plantilla=Template(contenidohtml)
+    
+    #crear contexto
+    contexto=Context(datos_contexto)
+    
+    #preparar documento para renderizar
+    documento_a_renderizar=plantilla.render(contexto)
+    
+    #devolver la respuesta al usuario
+    return HttpResponse(documento_a_renderizar)
